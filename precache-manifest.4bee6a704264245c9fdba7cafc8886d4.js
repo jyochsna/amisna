9579cf9010142f52f4e01f376e14ff3bd0e2ebad
jyochsna <55848802+jyochsna@users.noreply.github.com>
2020-06-26 17:08:42 -0700

    Updates

---
+++ precache-manifest.4bee6a704264245c9fdba7cafc8886d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9558f32bafbe5f0c2d148f024b60e0d",
+    "url": "/amisna/index.html"
+  },
+  {
+    "revision": "37fbd7513fbb2e7f06ea",
+    "url": "/amisna/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/amisna/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amisna/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37fbd7513fbb2e7f06ea",
+    "url": "/amisna/static/js/main.118da5eb.chunk.js"
+  },
+  {
+    "revision": "1a7e1e03ecb98a45ef49",
+    "url": "/amisna/static/js/runtime-main.d0d755d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amisna/static/media/logo.5d5d9eef.svg"
+  }
+]);
